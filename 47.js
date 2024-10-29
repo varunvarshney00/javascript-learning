3c25c231e0c6f357030272282326d34b5e478544
Varun Varshney <varunvarshney6882@gmail.com>
2024-10-29 11:07:23 +0530

    47 complete

---
+++ 47.js
@@ -0,0 +1,21 @@
+// const evenNumbers = [0, 2, 4, 6, 9, 8];
+
+
+
+// const res = evenNumbers.some((num, i) => {
+//     if(num % 2 === 1)
+//     {
+//         console.log('i->',i)
+//     }
+//     return num % 2 === 1
+// })
+
+
+
+
+
+// const res = evenNumbers.every((num, i) => {
+//     return num % 2 === 0
+// })
+
+// console.log(res)
